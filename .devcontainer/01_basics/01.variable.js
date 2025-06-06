6bc03a54a6c71119037d570454d32faac9be5f85
Subhadip Saha <subhasaha9976@gmail.com>
2025-06-06 05:58:17 +0000

    update

---
+++ .devcontainer/01_basics/01.variable.js
@@ -0,0 +1,5 @@
+const pi = 3.14;let radius = 5;
+var area = pi * radius * radius;
+msg = "Area of the circle is: ";
+console.log(msg + area);
+console.table([pi, radius, area]);
--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,7 +1,1 @@
-const pi = 3.14;
-let radius = 5;
-var area = pi * radius * radius;
-msg = "Area of the circle is: ";
-console.log(msg + area);
-console.table([pi, radius, area]);
-
+console.log("Welcome to Js.");
